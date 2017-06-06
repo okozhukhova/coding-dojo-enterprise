8a5e51d58fba68552a78cdb830ecb154b8b45a16
dmerz <merzdaniel@yahoo.de>
2017-06-06 14:13:07 +0200

    add csv-table coding-dojo in java and vbNet

---
+++ kata/function-kata/csv-table/VBNet/Main.vb
@@ -0,0 +1,11 @@
+﻿Module Main
+
+    Sub Main()
+
+        Dim csvTable As New CsvTable()
+        csvTable.DrawCsvToTerminal(String.Format("VeryBasicSzenario;With{0}NoHard;Case", vbCrLf))
+
+        Console.ReadKey() ' wait for user to end the program
+    End Sub
+
+End Module
