9ec5079c2f32a88162035efbedd8f37094283eff
oleksandra <oleksandra.kozhukhova@actano.de>
2017-07-10 16:36:49 +0200

    Übergabe von Parameter über Konsole

--- kata/function-kata/csv-table/VBNet/Main.vb
+++ kata/function-kata/csv-table/VBNet/Main.vb
@@ -1,11 +1,23 @@
 ﻿Module Main
 
     Sub Main()
+        Dim args As String() = Environment.GetCommandLineArgs()
+        For Each Arg In args
+            Console.WriteLine(Arg)
+        Next
+        Dim dateiname As String
+        If UBound(args) + 1 <> 2 Then
+            Console.WriteLine("Anzahl der Argumente stimmt nicht.")
+            dateiname = Console.ReadLine()
+            Console.WriteLine()
+        Else
+            dateiname = args(1)
+        End If
 
         Dim tabelle As New csv_table.csv.tabellieren.CsvTable
         'tabelle.DrawCsvToTerminal(String.Format("VeryBasicSzenario;With{0}NoHard;Case", vbCrLf))
-        tabelle.DrawCsvToTerminal("C:\Users\oleksandra\Desktop\test.txt")
-       
+        tabelle.DrawCsvToTerminal(dateiname)
+
         Console.ReadKey() ' wait for user to end the program
     End Sub
 
